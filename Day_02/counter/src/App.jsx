78d049f0ad366ec73cedc498a44bbe055c911348
Hammad Ali Khan <hammad.web3developer@gmail.com>
2024-04-18 06:43:21 +0500

    Use state hook

---
+++ Day_02/counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(0);
+  const addValue = () => {
+    if (count < 20) {
+      count = count + 1;
+      console.log(count);
+      setCount(count);}
+  };
+  const removeValue = () => {
+    if (count > 0) {
+      count = count - 1;
+      setCount(count);
+    }
+  };
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter Value :{count} </h2>
+      <button type="submit" onClick={addValue}>
+        add Value
+      </button>
+      <br></br>
+      <br></br>
+      <button type="submit" onClick={removeValue}>
+        remove Value
+      </button>
+    </>);
+}
+export default App;
