979e32583f0997b3bac0aae8d7b82259a140edac
Hammad Ali Khan <hammad.web3developer@gmail.com>
2024-04-30 23:51:26 +0500

    Mega Project

---
+++ Day_14/MegaProject/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  return (
+    <>
+      <div>Appwrite</div>
+    </>
+  )
+}
+
+export default App
