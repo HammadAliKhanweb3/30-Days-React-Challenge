16279abc5c4bd25ac04662c5d06a7e1310d42499
Hammad Ali Khan <hammad.web3developer@gmail.com>
2024-05-01 23:35:26 +0500

    Authentication

--- Day_14/MegaProject/config/config.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-  appWriteUrl: String((VITE_APP_APPWRITE_URL = "https://cloud.appwrite.io/v1")),
-  appWriteUrl: String((VITE_APP_PROJECT_ID = "66313599003ab4a8ba26")),
-  appWriteUrl: String((VITE_APP_DATABASE_ID = "66313719003d28808e16")),
-  appWriteUrl: String((VITE_APP_COLLECTION_ID = "663137600021e55353f3")),
-  appWriteUrl: String((VITE_APP_BUKKET_ID = "663138f1002cac9970a2")),
-};
-
-export default conf
