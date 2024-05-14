92b1f425c16797a797d98d87d011b4fb7d7b42df
Hammad Ali Khan <hammad.web3developer@gmail.com>
2024-05-14 23:08:42 +0500

    UseLayoutEffect

---
+++ Day_28/UseLayout/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useLayoutEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  useLayoutEffect(() => {
+    if (value === 0) setValue(5 + Math.random() * 50);
+  }, [value]);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button onClick={() => setValue(0)}>check</button>
+    </>
+  );
+}
+
+export default App;
+
+
+
